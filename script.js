83c8c49c0e320fd69061d73ba38ff6dd6c9ded27
Alan T Robi <alantrobi@gmail.com>
2025-03-31 23:34:36 +0530

    Registration Feature Added

--- script.js
+++ script.js
@@ -10,3 +10,52 @@ function countDownUpdate(){
     document.getElementById("countdown").innerHTML = val
 }
 setInterval(countDownUpdate,1000);
+
+function validateUsername() {
+    let name = document.getElementById("username").value;
+    if (name.trim() === "") {
+        alert("Name field cannot be empty!");
+        return false;
+    }
+    return true;
+}
+
+function validateEmail() {
+    let email = document.getElementById("email").value;
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(email)) {
+        alert("Invalid email format!");
+        return false;
+    }
+    return true;
+}
+
+function validatePassword() {
+    let password = document.getElementById("password").value;
+    let regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!regex.test(password)) {
+        alert("Password must be at least 8 characters, including a letter and a number.");
+        return false;
+    }
+    return true
+}
+
+function validatePhone() {
+    let phone = document.getElementById("phn").value;
+    let regex = /^\d{10}$/;
+    if (!regex.test(phone)) {
+        alert("Phone number must be exactly 10 digits.");
+        return false;
+    }
+    return true;
+}
+
+function validateForm(){
+    return validateUsername() && validateEmail() && validatePassword() && validatePhone()
+}
+
+function checkFields(){
+    if(validateForm()){
+        alert("Validation successful!")
+    }
+}
